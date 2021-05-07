eabb1b8287795066aac1ec51a3646e20925f532b
ezekielsung <ezekielsung@csus.edu>
2021-05-06 19:53:32 -0700

    Updates

---
+++ precache-manifest.877fe28ab8cebd5433358af6027b1b80.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dcc4d74637c167bc21ab9ad76e4a9d",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "acb3eb7b89b4b05a320b",
+    "url": "/personal-website/static/css/2.e58df67e.chunk.css"
+  },
+  {
+    "revision": "ce487a33a9b2c36f37f0",
+    "url": "/personal-website/static/css/main.8142fc1e.chunk.css"
+  },
+  {
+    "revision": "acb3eb7b89b4b05a320b",
+    "url": "/personal-website/static/js/2.f437ac6c.chunk.js"
+  },
+  {
+    "revision": "6bf55fedf38b4a4d0a2bdb04c2bbca57",
+    "url": "/personal-website/static/js/2.f437ac6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce487a33a9b2c36f37f0",
+    "url": "/personal-website/static/js/main.d8d67641.chunk.js"
+  },
+  {
+    "revision": "12a7fd022e23281f2159",
+    "url": "/personal-website/static/js/runtime-main.5fff88c0.js"
+  },
+  {
+    "revision": "70f40b0f362095d9544ebbd3c8293bf5",
+    "url": "/personal-website/static/media/best-app-development-tool.70f40b0f.md"
+  },
+  {
+    "revision": "bdea94e63d3da5dee4bbf391f6a0a24e",
+    "url": "/personal-website/static/media/common-misconceptions-about-payment.bdea94e6.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/personal-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/personal-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/personal-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/personal-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "78d248241b422acf5d590bcdda06139c",
+    "url": "/personal-website/static/media/things-to-know-about-startup-business.78d24824.md"
+  }
+]);
